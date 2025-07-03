97fa08a78ed0871c751e8baf198b26ef50eaf4fd
DangJem <dangjem0730@gmail.com>
2025-07-03 13:33:10 +0800

    making gene_track file from gff3

---
+++ examples/gff3/gff2gene_track.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+gff3FilesPath <- list.files(path = ".", pattern = "*.gff3")
+
+gff3Table <- map_df(gff3FilesPath,~read_tsv(.x,show_col_types = F,comment = "#",col_names = F) %>% set_names(c("seq_id", "source", "type", "start", "end", "score", "strand", "phase", "attributes")))
+
+geneTrackTable <- gff3Table %>% filter(type=="CDS") %>% mutate(anno=str_extract(attributes,"(?<=product=)[^;]+(?=;)")) %>% select(seq_id,start,end,anno)
+
+write_tsv(geneTrackTable,"gene_track.tsv")
